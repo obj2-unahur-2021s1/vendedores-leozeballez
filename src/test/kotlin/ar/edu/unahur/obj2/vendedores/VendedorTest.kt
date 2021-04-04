f05ac4281764247fd4c0f1de7dab6a2609ebe59f
leozeballez <zeballezleonel@hotmail.com>
2021-04-03 23:01:03 -0300

    faltan test etapa 3

--- src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
@@ -9,6 +9,8 @@ class VendedorTest : DescribeSpec({
   val sanIgnacio = Ciudad(misiones)
   val buenosAires = Provincia(17500000)
   val cordoba = Provincia(2000000)
+  val certDeproducto15 = Certificacion(true, 15)
+  val otraCertificacion10 = Certificacion(false, 10)
 
   describe("Vendedor fijo") {
     val obera = Ciudad(misiones)
@@ -26,6 +28,37 @@ class VendedorTest : DescribeSpec({
     it("esInfluyente") {
       vendedorFijo.esInfluyente().shouldBeFalse()
     }
+
+    describe("esVersatil") {
+      it("1 certificacion de productos y 1 que no es de productos)") {
+        vendedorFijo.agregarCertificacion(certDeproducto15)
+        vendedorFijo.agregarCertificacion(otraCertificacion10)
+        vendedorFijo.esVersatil().shouldBeFalse()
+      }
+
+      it("2 certificaciones sobre productos y 1 que no es sobre productos") {
+        //acá tenemos 1 certificacion de producto y otra que no es de producto
+        vendedorFijo.agregarCertificacion(certDeproducto15) //y le agregamos una mas de producto
+        vendedorFijo.esVersatil().shouldBeTrue()
+      }
+
+      it("3 certificaciones sobre productos") {
+        //tenemos 2 sobre productos y 1 que no
+        vendedorFijo.certificaciones.remove(otraCertificacion10) //quitamos la que no es de producto
+        vendedorFijo.agregarCertificacion(certDeproducto15) // agregamos la 3° certif. de producto
+        vendedorFijo.esVersatil().shouldBeFalse()
+      }
+
+      it("3 certificaciones que no son sobre productos") {
+        vendedorFijo.certificaciones.remove(certDeproducto15) //quitamos las 3 certif. de producto
+        vendedorFijo.certificaciones.remove(certDeproducto15)
+        vendedorFijo.certificaciones.remove(certDeproducto15)
+        vendedorFijo.agregarCertificacion(otraCertificacion10) //agregamos las 3 que no son de producto
+        vendedorFijo.agregarCertificacion(otraCertificacion10)
+        vendedorFijo.agregarCertificacion(otraCertificacion10)
+        vendedorFijo.esVersatil().shouldBeFalse()
+      }
+    }
   }
 
   describe("Viajante") {
@@ -41,8 +74,23 @@ class VendedorTest : DescribeSpec({
       }
     }
 
-    it("esInfluyente") {
+    it("NO esInfluyente") {
       viajante.esInfluyente().shouldBeFalse()
+    }
+
+    describe("esFirme") {
+      it("puntaje total de certificaciones >= 30") {
+        viajante.agregarCertificacion(certDeproducto15)
+        viajante.agregarCertificacion(certDeproducto15)
+        viajante.esFirme().shouldBeTrue()
+        viajante.certificaciones.remove(certDeproducto15)
+      }
+
+      it("puntaje total de certificaciones < 30") {
+        viajante.agregarCertificacion(otraCertificacion10)
+        viajante.esFirme().shouldBeFalse()
+        //tenemos 25 puntos ya que hay 1 certificacion de producto del anterior test
+      }
     }
   }
 
@@ -70,6 +118,16 @@ class VendedorTest : DescribeSpec({
 
       it("esInfluyente") {
         comercio.esInfluyente().shouldBeTrue()
+      }
+
+      describe("puedeTrabajarEn"){
+        it("una ciudad en la que tiene sucursal") {
+          comercio.puedeTrabajarEn(chivilcoy).shouldBeTrue()
+        }
+
+        it("una ciudad en la que NO tiene sucursal") {
+          comercio.puedeTrabajarEn(sanIgnacio).shouldBeFalse()
+        }
       }
     }
 
